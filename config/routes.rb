68eb784748c0ed0d34a4a20ce3232bf8df598919
Your Name <you@example.com>
2021-02-11 22:42:42 +0530

    pg gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home'
 get 'about', to: 'pages#about'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
