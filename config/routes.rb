1beafaaa6cb3e2dee074b0d9821200044eca2381
Your Name <you@example.com>
2021-02-11 22:51:56 +0530

    root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'home', to: 'pages#home'
+root 'pages#home'
 get 'about', to: 'pages#about'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
